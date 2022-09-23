a7e00f9bb277ce32edffe051acb32eaaffaf9b84
Victor Ramirez <vgerardo0528@hotmail.com>
2022-09-23 18:15:38 -0500

    archivos actualizados para Actividad 4

---
+++ README.md
@@ -0,0 +1,5 @@
+Este es el README para el proyecto de taller de API con Node.js
+
+Víctor Gerardo Ramírez González
+Universidad Autónoma de Querétaro
+Facultad de Informática
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var z = ;*/
 
 const express = require('express'); //require es para importar
 const app = express();
+const { pokemon } = require('./pokedex.json');
 
 /*
 Verbos HTTP: denotan una acción
@@ -19,12 +20,53 @@ app.get("/", (req, res, next) =>{
   //req es la petición del navegador
   //res respuesta clase objeto a la petición
 
+  //const pokemon = pokedex.pokemon;
   res.status(200); //
-  res.send("Hola mundo.");
+  res.send("Bienvenido al Pokedex");
 
 });
 
-app.listen(3000, ()=>{
+app.get('/pokemon/all', (req, res, next) =>{
+
+  //console.log(req.params.name);
+  res.status(200);
+  res.send(pokemon);
+
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>{
+
+  const id = req.params.id - 1;
+  if(id >= 0 && id <= 150){
+
+      res.status(200);
+      return res.send(pokemon[req.params.id - 1]);
+
+  }
+
+    res.status(404);
+    res.send("Pokémon no encontrado.")
+
+});
+
+app.get('/pokemon/:name', (req, res, next) =>{
+  const name = req.params.name;
+for (i=0; i<pokemon.length; i++){
+  if(pokemon[i].name==name){
+
+    res.status(200);
+    res.send(pokemon[i]);
+
+  };
+
+};
+
+    res.status(404);
+    res.send("Pokémon no encontrado.")
+
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
 
   console.log("Server is running...");
 
