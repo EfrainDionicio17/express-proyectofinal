e2bb2bc994f5c2b6405911578ad643bff7e7b667
Victor Ramirez <vgerardo0528@hotmail.com>
2022-11-06 14:56:05 -0600

    Activdad 9: POST

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,9 +2,33 @@ const express = require('express');
 const pokemon = express.Router();
 const db = require('../config/database');
 
-pokemon.post("/", (req,res,next)=>{
+pokemon.post("/", async (req,res,next)=>{
 
-  return res.status(200).send(req.body);
+  const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+  if(pok_name && pok_height && pok_weight && pok_base_experience){
+
+    let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+    query += ` VALUES('${pok_name}',${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+
+    const rows = await db.query(query);
+
+    console.log(rows);
+
+    if (rows.affectedRows == 1) {
+
+        return res.status(201).json({code:201, message: "Pokemon insertado correctamente."});
+
+    };
+
+    return res.status(500).json({code:500, message: "Ocurrió un error."});
+
+
+  };
+
+  return res.status(500).json({code:500, message: "Campos incompletos, revise de favor."});
+
+
 
 });
 
@@ -12,7 +36,7 @@ pokemon.get('/', async (req, res, next) =>{
 
   const pkmn = await db.query("SELECT * FROM pokemon");
   console.log(pkmn);
-  return res.status(200).json({code:1, message: pkmn});
+  return res.status(201).json({code:201, message: pkmn});
 
 });
 
@@ -24,7 +48,7 @@ pokemon.get('/:id([0-9]{1,3})', async (req, res, next) =>{
 
       const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = "+id+";");
       console.log(pkmn);
-      return res.status(200).json({code:1, message: pkmn});
+      return res.status(201).json({code:201, message: pkmn});
 
   }
 
@@ -41,7 +65,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) =>{
 
   if(pkmn_name.length>0) {
 
-    return res.status(200).json({ code:1, message: pkmn_name});
+    return res.status(201).json({ code:200, message: pkmn_name});
 
   };
 
