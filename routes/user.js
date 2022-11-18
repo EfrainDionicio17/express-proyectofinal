0357732cdaddf032fd00d1ac582dc8ccf867e7f6
Victor Ramirez <vgerardo0528@hotmail.com>
2022-11-17 20:59:51 -0600

    Actividad 12

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
+
 
 user.post("/", async (req, res, next) => {
 
@@ -14,6 +16,7 @@ user.post("/", async (req, res, next) => {
 
     if(rows.affectedRows == 1) {
 
+
         return res.status(200).json({code: 200, message: "Usuario creado con éxito."});
 
     }
@@ -25,6 +28,39 @@ user.post("/", async (req, res, next) => {
 
 });
 
+user.post("/login", async (req, res, next) => {
+
+  const {user_mail, user_password} = req.body;
+  const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+
+  const rows = await db.query(query);
+
+  if (user_mail && user_password) {
+
+    if(rows.length == 1){
+
+      const token = jwt.sign({
+
+            user_id: rows[0].user_id,
+            user_mail: rows[0].user_mail
+
+      }, "debugkey");
+
+      return res.status(200).json({code:200, message: token});
+
+    } else {
+
+      return res.status(401).json({code: 401, message: "Usuario y/o contraseña incorrectos."})
+
+    }
+
+  }
+
+  return res.status(500).json({code:500, message: "Campos incompletos."});
+
+
+
+});
 user.get("/", async (req, res, next) => {
 
   const query = "SELECT * FROM user";
@@ -33,5 +69,6 @@ user.get("/", async (req, res, next) => {
   return res.status(200).json({code:200, message: rows});
 
 });
+
 
 module.exports = user;
