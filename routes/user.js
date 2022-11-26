b3037c1b3a034970240030981619cccc741ce8f5
Victor Ramirez <vgerardo0528@hotmail.com>
2022-11-25 18:20:25 -0600

    Actividad 15

--- routes/user.js
+++ routes/user.js
@@ -50,7 +50,7 @@ user.post("/login", async (req, res, next) => {
 
     } else {
 
-      return res.status(401).json({code: 401, message: "Usuario y/o contraseña incorrectos."})
+      return res.status(200).json({code: 401, message: "Usuario y/o contraseña incorrectos."})
 
     }
 
