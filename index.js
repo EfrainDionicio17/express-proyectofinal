dba775fbf5b3a233fbef9d1e74a77b1e683b702a
Victor Ramirez <vgerardo0528@hotmail.com>
2022-11-30 18:31:21 -0600

    Actividad 16

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const index = require('./middleware/index');
 const cors = require('./middleware/cors');
 
 app.use(cors);
+
 app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
